2a5b9bd36d5fe85fc5791fbd4e57942874f52f98
fidbos <fidbos704@gmail.com>
2026-07-01 19:11:36 +0500

    Format and clean up urls.py

--- kundalik_loyiha/urls.py
+++ kundalik_loyiha/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('yozuvlar.urls')),  # Bu yozuvlar ilovasini ulaydi
+    path('', include('yozuvlar.urls')),  
 ]
 
